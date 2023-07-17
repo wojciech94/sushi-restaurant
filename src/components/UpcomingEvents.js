20b9c63cf1e20aa3eb82ae675265d3840b674592
Wojciech Kokoszka <wojciech.kokoszka94@gmail.com>
2023-07-17 20:27:35 +0200

    Update styles, preparing events section (upcomming events and workshops content and bg) (WiP)

--- src/components/UpcomingEvents.js
+++ src/components/UpcomingEvents.js
@@ -14,30 +14,40 @@ function UpcomingEvents() {
 	}
 
 	return (
-		<div className='w-100 bg-light position-relative z-index-0 p-8 overflow-hidden'>
+		<div className='w-100 bg-concrete position-relative z-index-0 p-8 overflow-hidden'>
+			<h2 className='d-flex text-white justify-center'>Upcomming events</h2>
 			<div
-				className='d-flex align-center g-4 mx-auto w-30 h-400px items-container'
+				className='d-flex align-center mx-auto w-30 items-container'
 				style={{ transform: `translateX(-${activeId * 100}%)` }}>
 				{eventsData != null &&
 					eventsData.map((ev, id) => {
 						return (
-							<div key={id} className='min-w-100 w-100 p-4 text-center'>
-								<h3>{ev.title}</h3>
-								<h3>{activeId}</h3>
-								<p>{ev.content[0]}</p>
-								<div>{ev.date}</div>
+							<div className='d-flex flex-center min-w-100 mt-4'>
+								<div key={id} className='border border-radius-5 bg-white p-8'>
+									<h3 className='mb-4'>{ev.title}</h3>
+									{ev.content.map(c => {
+										return <p className='mb-2'>{c}</p>
+									})}
+									<div className='d-flex justify-between mt-4'>
+										<div>{ev.date}</div>
+										<div>{ev.cost}</div>
+									</div>
+								</div>
 							</div>
 						)
 					})}
 			</div>
-			<div className='btn-group p-4 bg-white z-index-0'>
+			<div className='btn-group p-2 z-index-0'>
 				{eventsData != null &&
 					eventsData.map((ev, id) => {
 						return (
-							<div onClick={() => updateId(id)} className={id === activeId ? 'px-4 py-2 active' : 'px-4 py-2'}></div>
+							<div
+								onClick={() => updateId(id)}
+								className={id === activeId ? 'px-4 py-2 active' : 'bg-white px-4 py-2'}></div>
 						)
 					})}
 			</div>
+			<div className='position-absolute events-none top-0 left-0 w-100 h-100'></div>
 		</div>
 	)
 }
--- src/components/Workshops.js
+++ src/components/Workshops.js
@@ -3,8 +3,35 @@ import { React, Fragment } from 'react'
 function Workshops({ id }) {
 	return (
 		<Fragment>
-			<div id={id} className='bg-light position-relative z-index-0'>
-				Simple workshops div
+			<div id={id} className='workshops-image position-relative z-index-0 p-8'>
+				<div className='position-absolute bg-transparent-black-6 w-100 h-100 top-0 left-0'></div>
+				<div className='position-absolute top-20 left-40 rounded-4 p-8 bg-transparent-7'>
+					<div className='d-flex flex-column g-3 mb-4'>
+						<div>
+							<h2 className=''>Sushi workshops</h2>
+							<span className='badge'>Wrocław</span>
+						</div>
+						<div className='d-flex flex-column rounded-2 border-start-4 border-color-red bg-transparent-black-3 p-3'>
+							<div className=''>
+								Cost: <span className='fw-bold'>199 PLN</span> / person
+							</div>
+							<div>
+								Min number of participants: <span className='fw-bold'>8</span>
+							</div>
+						</div>
+					</div>
+					<h3 className=''>Workshop characteristics and schedule</h3>
+					<ul className='m-4'>
+						<li>You will be introduced to Japanese cuisine and learn about the history of sushi</li>
+						<li>Qualified specialists will show you how to prepare the most popular types of sushi</li>
+						<li>
+							You will prepare selected types of sushi on your own (under the supervision of specialists) (2 rolls /
+							person)
+						</li>
+						<li>You will learn how to use chopsticks and good table manners</li>
+						<li>You will participate in a joint tasting of prepared sets and traditional Japanese drinks</li>
+					</ul>
+				</div>
 			</div>
 		</Fragment>
 	)
